265860de57fb84976060bb4a3b24680b682b1bf4
gyosida <gianfranco.yosida@gmail.com>
2015-11-18 20:15:05 -0500

    handling audio pitch to simulate chipmunk and darth vader sounds

--- Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -13,6 +13,9 @@ class PlaySoundsViewController: UIViewController {
     
     var audioPlayer : AVAudioPlayer!
     var receivedRecordedAudio : RecordedAudio!
+    var audioEngine:AVAudioEngine!
+    var audioFile:AVAudioFile!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +28,8 @@ class PlaySoundsViewController: UIViewController {
 //        }
         audioPlayer = try! AVAudioPlayer(contentsOfURL: receivedRecordedAudio.filePathUrl)
         audioPlayer.enableRate = true
+        audioEngine = AVAudioEngine()
+        audioFile = try! AVAudioFile(forReading: receivedRecordedAudio.filePathUrl)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,6 +39,7 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func stopPlaying(sender: UIButton) {
         audioPlayer.stop()
+        audioEngine.stop()
     }
     
     @IBAction func makeFastPlay(sender: UIButton) {
@@ -44,11 +50,40 @@ class PlaySoundsViewController: UIViewController {
         playAudio(0.5)
     }
     
+    @IBAction func playChipmunkAudio(sender: UIButton) {
+        playAudioWithVariablePitch(1000)
+    }
+    
+    @IBAction func playDarthVaderAudio(sender: UIButton) {
+        playAudioWithVariablePitch(-1000)
+    }
+    
     func playAudio(rate : float_t) {
         audioPlayer.stop()
         audioPlayer.rate = rate
         audioPlayer.currentTime = 0.0
         audioPlayer.play()
+    }
+    
+    func playAudioWithVariablePitch(pitch: Float){
+        audioPlayer.stop()
+        audioEngine.stop()
+        audioEngine.reset()
+        
+        let audioPlayerNode = AVAudioPlayerNode()
+        audioEngine.attachNode(audioPlayerNode)
+        
+        let changePitchEffect = AVAudioUnitTimePitch()
+        changePitchEffect.pitch = pitch
+        audioEngine.attachNode(changePitchEffect)
+        
+        audioEngine.connect(audioPlayerNode, to: changePitchEffect, format: nil)
+        audioEngine.connect(changePitchEffect, to: audioEngine.outputNode, format: nil)
+        
+        audioPlayerNode.scheduleFile(audioFile, atTime: nil, completionHandler: nil)
+        try! audioEngine.start()
+        
+        audioPlayerNode.play()
     }
 
 }
